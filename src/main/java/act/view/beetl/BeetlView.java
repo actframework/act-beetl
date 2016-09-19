12692b4a0d3d24ca4948f9a9fde93b52b0305051
Gelin Luo <greenlaw110@gmail.com>
2016-09-19 11:23:47 +1000

    code format

--- src/main/java/act/view/beetl/BeetlView.java
+++ src/main/java/act/view/beetl/BeetlView.java
@@ -43,8 +43,8 @@ public class BeetlView extends View {
     @Override
     protected Template loadTemplate(String resourcePath, ActContext context) {
         _init();
-        if(beetl.getResourceLoader().exist(resourcePath)){
-        	return null;
+        if (beetl.getResourceLoader().exist(resourcePath)) {
+            return null;
         }
         org.beetl.core.Template template = beetl.getTemplate(resourcePath);
         return new BeetlTemplate(template, this);
