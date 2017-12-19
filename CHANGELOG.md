3cb20c8bb908e7b831a8b7f24fd0edeced6f545c
Gelin Luo <greenlaw110@gmail.com>
2017-12-19 22:15:12 +1100

    apply new build system

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Act-Beetl Change Log
+
+1.2.0
+* update to act-1.6.0
+
+1.1.0
+* update to act-1.5.0
+
+1.0.0
+- first formal release
+
+0.7.0
+- Update to actFramework 0.7
+
+0.6.0
+- Update to ActFramework 0.6
--- src/main/java/act/view/beetl/BeetlView.java
+++ src/main/java/act/view/beetl/BeetlView.java
@@ -1,5 +1,25 @@
 package act.view.beetl;
 
+/*-
+ * #%L
+ * ACT Beetl
+ * %%
+ * Copyright (C) 2017 ActFramework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import act.Act;
 import act.app.App;
 import act.view.Template;
@@ -16,10 +36,13 @@ import org.osgl.Osgl;
 import org.osgl.exception.ConfigurationException;
 import org.osgl.util.E;
 import org.osgl.util.S;
+import osgl.version.Version;
 
 import java.io.IOException;
 
 public class BeetlView extends View {
+
+    public static final Version VERSION = Version.get();
 
     public static final String ID = "beetl";
 
